8639ef7af812b09788fd95323f6fec1e0eef4910
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2022-05-02 16:09:41 -0700

    fetched and ccreated todos

---
+++ Todo.js
@@ -0,0 +1,125 @@
+const taskForm = document.getElementById("new-task-form")
+const taskInput = () => document.getElementById("new-task-input")
+const taskList = document.getElementById("tasks")
+
+
+const todos = []
+const baseURL = 'http://localhost:3000/todolists'
+
+
+document.addEventListener("DOMContentLoaded",() => {
+    loadTodos()
+    taskForm.addEventListener("submit", createTodo)
+})
+
+function loadTodos() {
+    console.log('a')
+
+    fetch(baseURL)
+        .then(resp => resp.json())
+        .then(data => {
+           displayTask(data)
+        })   
+}
+
+function displayTask(tasks){
+    tasks.forEach(task => displayTodos(task))
+}
+
+
+function displayTodos(task) {
+
+            const task_el = document.createElement("div")
+            task_el.classList.add("task")
+            const content = document.createElement("div")
+            content.classList.add("content")
+            task_el.appendChild(content)
+
+            const input_el = document.createElement("input")
+            input_el.classList.add("text")
+            input_el.type = "text"
+            // input_el.setAttribute('id', 'title')
+            input_el.value = task.content
+            input_el.setAttribute('readonly', 'readonly')
+            content.appendChild(input_el)
+            
+
+            const actions = document.createElement("div")
+            actions.classList.add("actions")
+
+
+            const delButton  = document.createElement("button")
+            delButton.classList.add("delete")
+            delButton.innerText = "Delete"
+            delButton.addEventListener("click", taskDelete)
+          
+
+
+            const edButton  = document.createElement("button")
+            edButton.classList.add("edit")
+            edButton.innerText = "Edit"
+            edButton.addEventListener("click", taskEdit)
+            
+
+            actions.appendChild(delButton)
+            actions.appendChild(edButton)
+         
+
+            task_el.appendChild(actions)
+            taskList.appendChild(task_el)
+            taskInput().value = ""
+
+}
+
+
+function createTodo (e) {
+    e.preventDefault() 
+            // const task = {
+            //     title: taskInput.value
+            // }
+
+            // todos.push(task)
+            // displayTodos(task)
+
+    const strongParams = {
+        todolist: {
+            content: taskInput().value
+        }
+    }
+    
+    let configObj = {
+        method: "post",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+      },
+        body: JSON.stringify(strongParams)
+    }
+
+    fetch(baseURL, configObj)
+    .then(resp => resp.json())
+    .then(data =>  displayTodos(data))
+
+}
+
+
+
+
+function taskEdit() {
+    inputElement = this.parentElement.parentElement.childNodes[0].getElementsByTagName("input").title
+   if(this.innerText.toLowerCase() == "edit"){
+      inputElement.removeAttribute("readonly");
+      inputElement.focus()
+      this.innerText = "Save";
+   } else {
+      inputElement.setAttribute("readonly", "readonly")
+      this.innerText = "Edit"
+   }
+}
+
+
+function taskDelete() {
+   currentTask = this.parentElement.parentElement
+   taskList.removeChild(currentTask)
+}
+
